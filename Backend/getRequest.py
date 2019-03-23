3225b75a89811cf3ac774dd2ba2d89db6766bf30
Michael James Radey <radeymichael@gmail.com>
2019-03-23 12:09:43 -0400

    Generated Api Resource
    Created an initial api resource in service.py

---
+++ Backend/getRequest.py
@@ -0,0 +1,2 @@
+from requests import put, get
+print(get('http://127.0.0.1:5000/').json())
--- Backend/service.py
+++ Backend/service.py
@@ -1,7 +1,20 @@
+import datetime
 from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+
 app = Flask(__name__)
-@app.route("/")
-def home():
-    return "Hello, World!"
+api = Api(app)
+
+#db = DB_objects.database()
+
+#@app.route("/")
+class HelloWorld(Resource):
+    def get(self):
+        return {'Hello': 'World'}
+
+# URL Management
+api.add_resource(HelloWorld, '/')
+
 if __name__ == "__main__":
     app.run(debug=True)
